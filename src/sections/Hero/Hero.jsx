68096344a2741ba87c4e3192473d42db1e29c4ef
Muhammad-Amir-DEV <amirsultan0321@gmail.com>
2025-12-02 18:12:08 +0500

    v 1.0.1

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -80,7 +80,7 @@ function Hero() {
           With a passion for developing modern React web apps for commercial
           businesses.
         </p>
-        <a href={CV} download>
+        <a href={#} download>
           <button className="hover">Resume</button>
         </a>
       </div>
